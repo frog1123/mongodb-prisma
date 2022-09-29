6cdd304485c41ad620d49fbc1fd44f7dd878739a
frog1123 <93895449+frog1123@users.noreply.github.com>
2022-09-29 18:14:52 -0500

    +

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,37 @@
 import { MongoClient } from 'mongodb';
 import express from 'express';
 import 'dotenv/config';
+import { PrismaClient } from '@prisma/client';
 
 // connect to database
 const client = new MongoClient(typeof process.env.DATABASE_URL === 'undefined' ? '' : process.env.DATABASE_URL);
 client.connect(() => console.log('connected to database'));
 const db = client.db();
+const prisma = new PrismaClient();
 
 const app = express();
 
-// insert object into frog collection with these properties
-db.collection('frog').insertOne({
-  name: 'joey',
-  smol: false,
-  habitat: ['rainforest', 'his house', 'outside']
-});
-
-app.get('/', async (req, res) => {
-  // find frog in frog collection with property name: 'joey'
-  const frog = await db.collection('frog').findOne({
-    name: 'joey'
+(async () => {
+  // insert object into frog collection with these properties
+  await prisma.frog.create({
+    data: {
+      name: 'connor',
+      smol: false,
+      habitat: ['rainforest', 'his house', 'outside']
+    }
   });
 
-  // send as response
-  res.send(frog);
-});
+  app.get('/', async (req, res) => {
+    // find frog in frog collection with property name: 'joey'
+    const frog = await prisma.frog.findFirst({
+      where: {
+        name: 'connor'
+      }
+    });
 
-app.listen(9000, () => console.log('api listening on port 9000'));
+    // send as response
+    res.send(frog);
+  });
+
+  app.listen(9000, () => console.log('api listening on port 9000'));
+})().finally(() => prisma.$disconnect());
